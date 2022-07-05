d7484e42c76d68a1ce781446c7e7cfbb945af54d
QcDev-arch <57719474+QcDev-arch@users.noreply.github.com>
2022-07-05 12:55:11 -0400

    Updated our test coverage to 97 % of our code and removed some stuff that shouldn't be there.

--- tests/moodle/moodle_fetch.test.js
+++ tests/moodle/moodle_fetch.test.js
@@ -2,8 +2,8 @@
 const MoodleActivity = require('../../app/models/moodle_activity');
 const MoodleAssignement = require('../../app/models/moodle_assignment');
 const MoodleQuiz = require('../../app/models/moodle_quiz');
-const { extractTar, fetchActivities, updateActivities, repackageToMBZ } = require("../../app/utils/xmlReader")
-const fs = require('fs')
+const { extractTar, fetchActivities, fetchQuizInfo, updateActivities, repackageToMBZ } = require("../../app/utils/xmlReader")
+const fs = require('fs-extra')
 const PATH = "data/backup-moodle2-course-1677-s20143-log792-09-20151102-1508-nu.mbz"
 const NEW_PATH = "tmp/backup-moodle2-course-1677-s20143-log792-09-20151102-1508-nu/"
 
@@ -13,8 +13,10 @@ describe('Moodle class instantiation test', () => {
         var activity = new MoodleActivity("test", "1", "1", "test_module", "file_path")
         expect(activity.title).toBe("test")
         expect(activity.moduleid).toBe("1")
+        expect(activity.getModuleId()).toBe("1")
         expect(activity.sectionid).toBe("1")
         expect(activity.modulename).toBe("test_module")
+        expect(activity.getModuleName()).toBe("test_module")
         expect(activity.directory).toBe("file_path")
     })
 
@@ -44,8 +46,10 @@ describe('Moodle class instantiation test', () => {
 // Test for XML Reader
 describe('Test for XML Reader', () => {
     beforeAll(() => {
+        fs.rmSync("./tmp", { recursive: true });
+        fs.rmSync("./mbzPackages", { recursive: true});
         // Extract the moodle backup file before doing these tests
-        return extractTar(PATH);
+        extractTar(PATH);
     });
 
     test('Modification of a quiz', () => {
@@ -58,16 +62,14 @@ describe('Test for XML Reader', () => {
 
         let newactivities = fetchActivities(NEW_PATH);
         expect(newactivities[1].getTimeOpen()).toBe(time);
-    })
-    // TODO check why it fails later
-    // test('Repackage as an mbz file', () => {
-    //     return repackageToMBZ(NEW_PATH).then((mbzPath) => {
-    //         console.log(mbzPath)
-    //         // Promise added to the repackageToMBZ function for the test to wait for the file to exist.
-    //         expect(fs.existsSync(mbzPath)).toBeTruthy();
-    //         // Delete the test file created to prevent having a lot of backup from tests.
-    //         fs.unlinkSync(mbzPath);
+    });
 
-    //     });
-    // })
+    test('Repackage as an mbz file', () => {
+        return repackageToMBZ(NEW_PATH).then((mbzPath) => {
+            // Promise added to the repackageToMBZ function for the test to wait for the file to exist.
+            expect(fs.existsSync(mbzPath)).toBeTruthy();
+            // Delete the test file created to prevent having a lot of backup from tests.
+            fs.unlinkSync(mbzPath);
+        });
+    });
 })
