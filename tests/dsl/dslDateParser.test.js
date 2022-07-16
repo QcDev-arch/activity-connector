325d61d20d8e2a5af03b1659232bb43280a21226
QcDev-arch <57719474+QcDev-arch@users.noreply.github.com>
2022-07-16 13:48:21 -0400

    Add Mocking for dslDateParser tests

--- tests/dsl/dslDateParser.test.js
+++ tests/dsl/dslDateParser.test.js
@@ -5,13 +5,26 @@ const DslParser = require("../../app/utils/dslParser");
 const fs = require("fs-extra");
 var dslString;
 var activities;
-let ical = new iCalParser("", "LOG210", "01", "2022", 2);
+
+const activityTypes = ["", "C", "TP", "Labo"];
+const symbol = "LOG210";
+const group = "01";
+const year = "2022";
+const semesterSeason = 2;
+
+jest.mock("../../app/utils/iCalParser");
+
+// Mocking require that we reassigned all the previous values
+const icsParser = new iCalParser(activityTypes[0], symbol, group, year, semesterSeason);
+
+const fakeICSReturn = require('../../data/fakeData');
 
 /*---------DSL DATE PARSER TEST----------*/
 
 beforeAll(async () => {
   dslString = fs.readFileSync("./data/test.dsl", { encoding: "utf8" });
-  activities = await ical.parse();
+  icsParser.parse.mockResolvedValue(fakeICSReturn);
+  activities = await icsParser.parse();
 });
 
 test("Get the list of modified times", () => {
@@ -23,35 +36,30 @@ test("Get the list of modified times", () => {
   const testTimes = [
     {
       activity: "Moodle Quiz 1",
-      open: new Date("2022-05-05T16:00:00.000Z"),
-      close: new Date("2022-05-12T12:00:00.000Z"),
+      open: "2022-05-05T16:00:00.000Z",
+      close: "2022-05-12T12:00:00.000Z",
     },
     {
       activity: "Moodle Quiz 2",
-      open: new Date("2022-05-12T16:00:00.000Z"),
-      close: new Date("2022-05-19T03:55:00.000Z"),
+      open: "2022-05-12T16:00:00.000Z",
     },
     {
       activity: "Moodle Homework 1",
-      open: new Date("2022-05-17T15:30:00.000Z"),
-      due: new Date("2022-05-24T03:55:00.000Z"),
-      cutoff: new Date("2022-05-24T03:55:00.000Z"),
+      open: "2022-05-17T15:30:00.000Z",
+      due: "2022-05-24T03:55:00.000Z",
+      cutoff: "2022-05-24T03:55:00.000Z",
     },
-    { activity: "Exam 1", open: new Date("2022-07-14T12:30:00.000Z") },
+    { activity: "Exam 1", open: "2022-07-14T12:30:00.000Z" },
   ];
 
-  expect(newTimes).toEqual(expect.anything());
+  expect(newTimes[0].open.toISOString()).toStrictEqual(testTimes[0].open);
+  expect(newTimes[0].close.toISOString()).toStrictEqual(testTimes[0].close);
 
-  // TODO : Find a way to use iCalParser with a predownload ics file instead of fetching on the ETS portal
-  // expect(newTimes[0].open).toStrictEqual(testTimes[0].open);
-  // expect(newTimes[0].close).toStrictEqual(testTimes[0].close);
+  expect(newTimes[1].open.toISOString()).toStrictEqual(testTimes[1].open);
 
-  // expect(newTimes[1].open).toStrictEqual(testTimes[1].open);
-  // expect(newTimes[1].close).toStrictEqual(testTimes[1].close);
+  expect(newTimes[2].open.toISOString()).toStrictEqual(testTimes[2].open);
+  expect(newTimes[2].due.toISOString()).toStrictEqual(testTimes[2].due);
+  expect(newTimes[2].cutoff.toISOString()).toStrictEqual(testTimes[2].cutoff);
 
-  // expect(newTimes[2].open).toStrictEqual(testTimes[2].open);
-  // expect(newTimes[2].close).toStrictEqual(testTimes[2].close);
-  // expect(newTimes[2].cutoff).toStrictEqual(testTimes[2].cutoff);
-
-  // expect(newTimes[3].open).toStrictEqual(testTimes[3].open);
+  expect(newTimes[3].open.toISOString()).toStrictEqual(testTimes[3].open);
 });
